215ff4f8391a1f35c82a4edfbe1b35c390cc6607
shams1987 <shamsjshaikh@gmail.com>
2023-09-14 17:07:38 -0500

    added users app

---
+++ airline/users/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+# Create your views here.
+
+def index(request):
+# If no user is signed in, return to login page: 
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login")) 
+    return render(request, "users/user.html")
+
+def login_view(request):
+    return render(request, "users/login.html")
+
+def logout_view(request):
+    # Pass is a simple way to tell python to do nothing. 
+    pass
